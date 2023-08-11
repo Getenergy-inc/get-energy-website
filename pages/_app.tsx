88fd6994d5cfd522e2984031a772646e4a4c6400
DanielBello7 <gokebello@gmail.com>
2023-08-11 22:40:47 +0100

    chore: Addition of Font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import type { AppProps } from 'next/app';
 import Layout from '@/components/_layout';
 import '@/styles/globals.css';
+import localFont from 'next/font/local';
+
+const satoshi = localFont({ src: '../assets/fonts/Satoshi-Regular.otf' });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <div className={satoshi.className}>
+        <Component {...pageProps} />
+      </div>
     </Layout>
   )
 }
