c4b132fc93b5350c865f403519b7f8feb211e264
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2023-12-11 13:36:42 +0100

    update: contact page

--- features/contact-us/index.tsx
+++ features/contact-us/index.tsx
@@ -5,15 +5,7 @@ import Form from "./form";
 
 export default function ContactUs() {
   const body = `
-    We're here to help. Welcome to the Get 
-    Energy Contact Us page, your gateway 
-    to seamless support. Whether you have questions, 
-    feedback, or need assistance, our dedicated 
-    team is ready to assist you. Reach out to us 
-    through our contact form, phone, or email, 
-    and we'll ensure a prompt and helpful response. 
-    We value your satisfaction and look forward to 
-    hearing from you.
+  Whether you have questions, feedback, or need assistance, our dedicated team is ready to assist you. Reach out to us through our contact form, phone, or email, and we'll ensure a prompt and helpful response. We value your satisfaction and look forward to hearing from you.
   `;
 
   return (
